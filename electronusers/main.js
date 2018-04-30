7c94164682ee38b3d3692452b070dfda68417850
MARIO EDUARDO CHAIDEZ MORALES <13170237@itculiacan.edu.mx>
2018-04-30 11:58:54 -0600

    Primer Pantalla con electron

---
+++ electronusers/main.js
@@ -0,0 +1,24 @@
+//Cargamos la aplicacion de electron
+const app = require('electron').app;
+//Para crear ventanas del sistema operartivo
+const BrowserWindow = require('electron').BrowserWindow;
+const path = require('path');
+const url = require('url');
+//let es una constante
+let PantallaPrincipal;
+
+function muestraPantallaPrincipal()
+{
+	//Creamos una pantalla vacía
+	PantallaPrincipal = new BrowserWindow({width:620,height:825});
+	//Le damos contenido a esa pantalla
+	PantallaPrincipal.loadURL(url.format({
+		pathname: path.join(__dirname,'index.html'),
+		protocol: 'file',
+		slashes: true
+	}));
+	//Mostramos la pantalla
+	PantallaPrincipal.show();
+}
+
+app.on('ready',muestraPantallaPrincipal);
