1a6e229d94464fe874ad6ce0115ba996a47c03ba
MARIO EDUARDO CHAIDEZ MORALES <13170237@itculiacan.edu.mx>
2018-05-04 12:00:40 -0600

    ElectronUsers terminado

---
+++ electronusers/index.js
@@ -0,0 +1,22 @@
+var $ = require('jquery');
+var inicia = function()
+{
+	var nuevo = function()
+	{
+		//JASON= JavaScript Object 
+		$.ajax(
+		{
+  			url: 'https://randomuser.me/api/',
+  			dataType: 'json',
+  			success: function(data) 
+  			{
+    			$("#nombre").html(data.results[0].name.first+" "+data.results[0].name.last);
+    			$("#foto").attr("src",data.results[0].picture.large);
+  			}
+		});
+	}
+	//alert("Página cargada");
+	$("#btnNuevo").on("click",nuevo);//Activar el boton.
+	//$("#btnNuevo").on("click",nuevo);Desactivar el boton.
+}
+$(document).ready(inicia);
