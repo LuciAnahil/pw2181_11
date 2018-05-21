85d4c2f68c7d50604fdd769087649070f168cc2d
Mario Chaidez <13170237@itculiacan.edu.mx>
2018-05-21 11:54:55 -0600

    Proyecto

---
+++ Servidor/Primera.php
@@ -0,0 +1,10 @@
+<?php//Inicio Php
+//include()
+//require()
+	for ($i=0; $i <= 10; $i++) 
+	{ 
+		//echo- Se cutiliza para imprimir cuando hay un problema en navegador y consola NO UTILIZAR
+		print("Texto ".$i." <br>"); // Es el que se debe de utilizar para que solo imprima en consola
+	}
+?><!--Fin PHP
+
