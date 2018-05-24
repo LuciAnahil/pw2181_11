2515b1ccd2b8c1f82d8ddccc2890867e40b2dddd
Mario Chaidez <13170237@itculiacan.edu.mx>
2018-05-24 12:08:13 -0600

    Busca usuario

--- Servidor/js/eventos.js
+++ Servidor/js/eventos.js
@@ -20,7 +20,10 @@ var inicioApp = function()
 				if(response.respuesta == true)
 				{
 					$("#secInicio").hide("slow");
+					$("#frmUsuarios").show("slow");
 					//alert("Bienvenido");
+					//Posiciona el cursor en el cuadro de texto
+					$("#txtNombreUsuario").focus();
 				}
 				else
 				{
@@ -33,6 +36,47 @@ var inicioApp = function()
 			}
 		});
 	}
+	var buscaUsuario = function()
+	{
+		var usuario = $("#txtNombreUsuario").val();
+		var parametros = "opc=buscaUsuario"+
+						 "&usuario="+usuario+
+						 "&aleatorio="+Math.random();
+		if(usuario != "")
+		{
+			$.ajax({
+				cache:false,
+				type:"POST",
+				dataType:"json",
+				url:"php/buscaUsuario.php",
+				data: parametros,
+				success:function(response){
+					if(response.respuesta == true)
+					{
+						$("#txtNombreUsuario").val(response.nombre);
+						$("#txtClaveUsuario").val(response.clave);
+					}
+					else
+					{
+						$("#txtNombre").focus();
+					}
+
+				},
+				error:function(xhr,ajaxOptions,thrownError)
+				{
+				}	
+			});
+		}
+	}
+	var teclaNombreUsuario = function(tecla)
+	{
+		if(tecla.which == 13)
+		{
+			buscaUsuario();
+		}
+	}
 	$("#btnAceptar").on("click",Aceptar);
+	$("#txtNombreUsuario").on("keypress",teclaNombreUsuario);
+	$("#frmUsuarios").hide();
 }
 $(document).ready(inicioApp);
